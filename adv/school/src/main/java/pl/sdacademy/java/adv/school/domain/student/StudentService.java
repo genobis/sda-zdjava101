876f44bbacc55fb74d8f0aae566c2acaab4b7530
Cytawa <tomasz.poniatowski@cytawa.com>
2022-01-12 18:58:02 +0100

    zmiana w readme

--- adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/student/StudentService.java
+++ adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/student/StudentService.java
@@ -2,25 +2,18 @@ package pl.sdacademy.java.adv.school.domain.student;
 
 import pl.sdacademy.java.adv.school.domain.student.model.Student;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StudentService {
     private final StudentRepository studentRepository;
 
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository)
+    {
         this.studentRepository = studentRepository;
     }
 
     public List<Student> getStudentsSortedByCityAndName() {
 
-        List<Student> allStudents = studentRepository.findAllStudents();
-
-        return allStudents.stream()
-                .sorted(Comparator.comparing(Student::getCity)
-                        .thenComparing(Student::getLastName)
-                        .thenComparing(Student::getFirstName))
-                .collect(Collectors.toUnmodifiableList());
+        throw new UnsupportedOperationException();
     }
 }
